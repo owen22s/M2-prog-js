83dd84bb3ad2f7044223090265d16756b278b66e
Owen Stas <34884@ma-web.nl>
2022-11-24 10:31:14 +0100

    ingeleverd

---
+++ les1/app.js
@@ -0,0 +1,25 @@
+class App
+{
+    runAplication()
+    {
+        console.log("hello world");
+        // code gaat hier!
+        const appNaam = ("Crazy Aplication");
+        console.log (appNaam)
+        let versienummer = ("0.9");
+        console.log (versienummer) 
+        let versiedatum =  ("24-11-2022"); 
+        console.log (versiedatum) 
+        const autheur = ("Owen");
+        console.log (autheur) 
+        let copyright = ("2022");
+        console.log(copyright)
+        let distributeur = ("Owen inc");
+        console.log(distributeur)
+        let darkmode = (false)
+        console.log(darkmode)
+    }
+}
+let app = new App
+app.runAplication()     
+
